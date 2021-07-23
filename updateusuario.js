da5b215634923f7ff7aa1d1e437ec86a4ac157f7
krincon36 <76145970+krincon36@users.noreply.github.com>
2021-07-23 12:10:00 -0500

    fix/updateusuario.js

--- updateusuario.js
+++ updateusuario.js
@@ -1,25 +1,32 @@
-app.post('/PostUsuario', function(req, res) {
+app.post('/PostUsuario', function (req, res) {
   console.log(req.headers["secret"]);
   console.log(req.body);
   console.log(req.body.email);
-  sql= "insert into usuario(email,password,rol) values('"+req.body.email+"','"+req.body.password+"','"+req.body.rol+"');",
-  connection.query(sql, function (err, result) {  
-    if (err) 
-    {
-        sql= "update usuario set password = '"+req.body.password+"',rol='"+req.body.rol+"' where email='"+req.body.email+"';",
-        connection.query(sql, function (err, result) {  
-        if (err) 
-        {
-          console.log( err);}
-        else{
-          console.log("1 record updated");  
-          res.json([{"resultado":"ok"}]);
-        }      
-        });
-    }
-    else{
-      console.log("1 record inserted");  
-      res.json([{"id":result.insertId}]);
-    }  
-    
-    });  
+  getUsuarioHeaderData(req);
+  if (usuariorol == "ADMIN") {//solo los admin pueden crear usuarios
+    sql = "insert into usuario(email,password,rol) values('" + req.body.email + "','" + req.body.password + "','" + req.body.rol + "');",
+      connection.query(sql, function (err, result) {
+        if (err) {
+          sql = "update usuario set password = '" + req.body.password + "',rol='" + req.body.rol + "' where email='" + req.body.email + "';",
+            connection.query(sql, function (err, result) {
+              if (err) {
+                console.log(err);
+              }
+              else {
+                console.log("1 record updated");
+                res.json([{ "resultado": "ok" }]);
+              }
+            });
+        }
+        else {
+          console.log("1 record inserted");
+          res.json([{ "id": result.insertId }]);
+        }
+
+      });
+  }
+  else {
+    console.log("NO ES ADMIN");
+    res.json([{ "id": 0, "ERROR": "NO ES ADMIN" }]);
+  }
+});
