beff5d3472009f973572a74bab537a7b16c56a26
krincon36 <76145970+krincon36@users.noreply.github.com>
2021-07-23 11:48:13 -0500

    fix/conection js

--- conection.js
+++ conection.js
@@ -1,14 +1,11 @@
-var mysql = require('mysql');
-
 var connection = mysql.createConnection({
-    host: "webservice.southcentralus.cloudapp.azure.com",
-    user: "root",
-    password: "Genio042019@",
-    database:"tarea"
-  });
+  host: "127.0.0.1",
+  user: "dev02",
+  password: "Best042021@",
+  database: "tarea"
+});
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
 });
-  
